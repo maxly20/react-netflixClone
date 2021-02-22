e6f61cbf991c938e5f0153724574327a95f1329f
maxly20 <maxly2011@gmail.com>
2021-02-23 10:08:21 +1100

    First Commit

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://i.ibb.co/S61cpbx/Netpickr-Logo.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
